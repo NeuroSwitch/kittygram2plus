9c80e1126fd2c214cb4c69e3c4e6d2242cf8bde7
Artur Muratov <a.muratov86@yandex.ru>
2023-05-09 21:47:57 +0500

    Commit

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,58 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
+from .permissions import OwnerOrReadOnly, ReadOnly
+
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
+
+from .throttling import WorkingHoursRateThrottle
+
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
+from .pagination import CatsPagination
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters
+
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
+    # Если он вернёт False - все запросы будут отклонены
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
+    throttle_scope = 'low_request'
+    # pagination_class = PageNumberPagination
+    # pagination_class = LimitOffsetPagination
+    # Вот он наш собственный класс пагинации с page_size=20
+    # pagination_class = CatsPagination
+
+    # # Указываем фильтрующий бэкенд DjangoFilterBackend
+    # Из библиотеки django-filter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    # Временно отключим пагинацию на уровне вьюсета, 
+    # так будет удобнее настраивать фильтрацию
+    pagination_class = None
+    # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year')  
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    # search_fields = ('achievements__name', 'owner__username')
+    ordering = ('birth_year',)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернем обновленный перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions() 
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
